d5b8906c4377ec217a2b9e038b97eebdae9a9bca
wilkneMaia <wilkne@outlook.com>
2020-10-21 23:23:55 -0300

    chore: update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,23 +1,15 @@
+const { compilerOptions } = require('./tsconfig.json')
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+
 module.exports = {
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
+  clearMocks: true,
+  preset: 'ts-jest',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
+  projects: [
+    "<rootDir>/packages/server/**/jest.config.js"
   ],
-
-  // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage',
-
-  // A list of paths to directories that Jest should use to search for files in
-  roots: [
-    '<rootDir>/src'
-  ],
-
-  // The test environment that will be used for testing
-  testEnvironment: 'node',
-
-  // A map from regular expressions to paths to transformers
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
-}
+  testEnvironment: "node",
+  testMatch: [
+    "*.spec.ts"
+  ]
+};
